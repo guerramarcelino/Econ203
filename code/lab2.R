befc0be8a43b669810ea59bbbca0c7d17edd191c
Marcelino Guerra <marcelinobguerra@gmail.com>
2020-01-30 09:14:08 -0600

    lab2new

--- code/lab2.R
+++ code/lab2.R
@@ -1,20 +1,19 @@
 #################### HW2 HTR Problem #12 ##############################
+#### Ho p<=.15
+#### Ha p>.15
 
-#### Ho p<=.12
-#### Ha p>.12
-
-p<-0.12
-### Sample with 200 observations
-n<-200
+p<-0.15
+### Sample with 400 observations
+n<-400
 ##### The point estimator
-phat<-33/200
+phat<-58/400
 phat
 
 ##### Use the formula to calculate z
 z<-(phat-p)/sqrt(p*(1-p)/n)
 z
 
-##### The critical value associated with the level of confidence (.10). Remember NORMSINV?
+##### The critical value associated with the level of significance (.10). Remember NORMSINV?
 zc<-qnorm(0.9)
 zc
 ###### Is your z>zc? If yes, reject Ho. Let's play with ifelse statements!
@@ -55,7 +54,7 @@ install.packages("xlsx")
 require(xlsx)
 
 ################## Importing data from an excel file. Remember to download the attached file debtequityv1
-debteq<-read.xlsx("debtequityv1.xls", sheetName = "Sheet1", as.data.frame = T, header = F)
+debteq<-read.xlsx("debtequityv3.xls", sheetName = "Sheet1", as.data.frame = T, header = F)
 View(debteq)
 summary(debteq)
 
@@ -89,14 +88,13 @@ min
 max<-mean+tc*(sd/sqrt(15))
 max
 
-########### Try the new interval with 200 observations - you have to change tc (the critical t) and sqrt(n) in the formula
+########### Try the new interval with 450 observations - you have to change tc (the critical t) and sqrt(n) in the formula
 
 ########## Finally, in order to get n, 
 
-zc<-qnorm(0.995)
+zc<-qnorm(0.95)
 zc
 
-######### Remember: you have to divide alfa by 2 - so you have 0.01/2=0.005. qnorm(1-0.005) -> qnorm(0.995)
-w<-0.01
+w<-0.015
 n<-(sd)^2*(zc)^2/w^2
 n
